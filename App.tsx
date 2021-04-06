fe847dfc70302855e0b2ca3d87ea86090ca22ec8
wangshicheng <wangshicheng@bytedance.com>
2021-04-06 17:28:48 +0800

    feat(project): init project

---
+++ App.tsx
@@ -0,0 +1,29 @@
+/*
+ * @Author: wangshicheng
+ * @Date: 2021-04-04 10:44:20
+ * @LastEditTime: 2021-04-06 17:15:37
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /MusicProject/App.tsx
+ */
+/**
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+const App = () => {
+  return (
+    <View>
+      <Text>HOME</Text>
+    </View>
+  );
+};
+
+// const styles = StyleSheet.create({});
+
+export default App;
